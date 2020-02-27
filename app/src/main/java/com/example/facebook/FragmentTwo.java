526bcc8c288a1e63fadbf87c2713142a8cd5cfd9
vidu-uni <amila.viduruwan96@gmail.com>
2020-02-28 00:22:04 +0530

    search3

--- app/src/main/java/com/example/facebook/FragmentTwo.java
+++ app/src/main/java/com/example/facebook/FragmentTwo.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -13,16 +14,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.facebook.data.MyData;
-import com.example.myfb.database.Mydatabase;
 
-import static com.example.myfb.database.Mydatabase.DB_NAME;
-import static com.example.myfb.database.Mydatabase.DB_VERSION;
 
 public class FragmentTwo extends Fragment {
     MyData mydb;
     SearchView search;
     TextView name,age,mark;
     String result;
+    EditText type;
     public static FragmentTwo newInstance(){
         FragmentTwo fragmentTwo=new FragmentTwo();
         return fragmentTwo;
@@ -36,36 +35,21 @@ public class FragmentTwo extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mydb = new MyData(getContext(), DB_NAME, null, DB_VERSION);
+        mydb = new MyData(getContext(), MyData.DB_NAME, null, MyData.DB_VERSION);
 
-        searchView = view.findViewById(R.id.searchView);
+        search= view.findViewById(R.id.search);
+        type= view.findViewById(R.id.type);
         name = view.findViewById(R.id.name);
         age = view.findViewById(R.id.age);
         mark = view.findViewById(R.id.mark);
 
-        result = searchView.toString();
-
-        searchView.setOnSearchClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Cursor rs = mydb.getAll();
-
-                if((rs.getCount())==0){
-
-                    return;
-                }
-
-
-
-                List itemIds = new ArrayList<>();
-                while(cursor.moveToNext()) {
-                    long itemId = cursor.getLong(
-                            cursor.getColumnIndexOrThrow(FeedEntry._ID));
-                    itemIds.add(itemId);
-                }
-                cursor.close()
-            }
-        });
+        List itemIds = new ArrayList<>();
+        while(data.moveToNext()) {
+            long itemId = data.getLong(
+                    data.getColumnIndexOrThrow(FeedEntry._ID));
+            itemIds.add(itemId);
+        }
+        data.close();
 
 
     }
