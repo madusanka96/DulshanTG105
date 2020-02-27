a578d444326a8df451c582713a005656813e7e0a
Dulshan <dpmadusanka96@gmail.com>
2020-02-27 18:01:31 +0530

    Tests

--- app/src/main/java/com/example/facebook/MainActivity.java
+++ app/src/main/java/com/example/facebook/MainActivity.java
@@ -10,7 +10,7 @@ import com.google.android.material.tabs.TabLayout;
 public class MainActivity extends AppCompatActivity {
 
        ViewPager viewPager;
-
+//declare
        MyFragmentAdapter adapter;
        TabLayout tabLayout;
 
