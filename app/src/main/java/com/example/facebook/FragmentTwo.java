618d325c27c17974d4ea7cbf74be354d6b75d9e6
thalindu <wdtlakshan@gmail.com>
2020-02-28 07:06:35 +0530

    Merge branch 'master' of C:\Users\Thalindu\Documents\ass\FaceBookNew with conflicts.

--- app/src/main/java/com/example/facebook/FragmentTwo.java
+++ app/src/main/java/com/example/facebook/FragmentTwo.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
 
@@ -14,14 +13,16 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.facebook.data.MyData;
+import com.example.myfb.database.Mydatabase;
 
+import static com.example.myfb.database.Mydatabase.DB_NAME;
+import static com.example.myfb.database.Mydatabase.DB_VERSION;
 
 public class FragmentTwo extends Fragment {
     MyData mydb;
-    SearchView search;
+    SearchView searchView;
     TextView name,age,mark;
     String result;
-    EditText type;
     public static FragmentTwo newInstance(){
         FragmentTwo fragmentTwo=new FragmentTwo();
         return fragmentTwo;
@@ -35,21 +36,36 @@ public class FragmentTwo extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mydb = new MyData(getContext(), MyData.DB_NAME, null, MyData.DB_VERSION);
+        mydb = new MyData(getContext(), DB_NAME, null, DB_VERSION);
 
-        search= view.findViewById(R.id.search);
-        type= view.findViewById(R.id.type);
+        searchView = view.findViewById(R.id.searchView);
         name = view.findViewById(R.id.name);
         age = view.findViewById(R.id.age);
         mark = view.findViewById(R.id.mark);
 
-        List itemIds = new ArrayList<>();
-        while(data.moveToNext()) {
-            long itemId = data.getLong(
-                    data.getColumnIndexOrThrow(FeedEntry._ID));
-            itemIds.add(itemId);
-        }
-        data.close();
+        result = searchView.toString();
+
+        searchView.setOnSearchClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor rs = mydb.getAll();
+
+                if((rs.getCount())==0){
+
+                    return;
+                }
+
+
+
+                List itemIds = new ArrayList<>();
+                while(cursor.moveToNext()) {
+                    long itemId = cursor.getLong(
+                            cursor.getColumnIndexOrThrow(FeedEntry._ID));
+                    itemIds.add(itemId);
+                }
+                cursor.close()
+            }
+        });
 
 
     }
